d068a265a6acf3cea5f53b8f1a2e4575b4a2f4b6
adarrssh <adarsh00502@gmail.com>
2024-09-03 20:41:07 +0530

    refactor:refactor

--- src/components/Challenges/DropDownButton.js
+++ src/components/Challenges/DropDownButton.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Menu, MenuItem, Checkbox, ListItemText, Backdrop, Box, Icon, Container, Typography, Divider } from '@mui/material';
+import { Button, Menu, MenuItem, Checkbox, ListItemText, Backdrop, Container, Typography, Divider } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
@@ -31,7 +31,19 @@ export default function DropdownWithBackdrop() {
 
     return (
         <>
-            <Button variant="contained" onClick={handleClick} sx={{ display: 'flex', backgroundColor: 'white', width: '130px', height: '50px', color: 'black', textTransform: 'none', borderRadius: '12px' }}>
+            <Button
+                variant="contained"
+                onClick={handleClick}
+                sx={{
+                    display: 'flex',
+                    backgroundColor: 'white',
+                    width: '130px',
+                    height: '50px',
+                    color: 'black',
+                    textTransform: 'none',
+                    borderRadius: '12px',
+                }}
+            >
                 <Container>
                     <p style={{ fontSize: '18px' }}>Filter</p>
                 </Container>
@@ -41,11 +53,18 @@ export default function DropdownWithBackdrop() {
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
-                MenuListProps={{ 'aria-labelledby': 'dropdown-button', borderTopLeftRadius: '12px', borderTopRightRadius: '12px' }}
-
+                MenuListProps={{
+                    'aria-labelledby': 'dropdown-button',
+                }}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
             >
-
-
                 <MenuItem
                     disableGutters
                     sx={{ width: '300px', fontSize: '18px', height: '50px' }}
@@ -61,32 +80,27 @@ export default function DropdownWithBackdrop() {
 
                 <Divider />
 
-
                 <MenuItem sx={{ width: '300px', display: 'flex' }}>
                     <Container disableGutters sx={{ paddingLeft: '12px' }}>
                         <Typography variant="h6" sx={{ fontSize: '16px', fontWeight: 'light' }}>Status</Typography>
                     </Container>
                 </MenuItem>
 
-
-                {['All', 'active', 'Upcoming', 'Past'].map((option) => (
+                {['All', 'Active', 'Upcoming', 'Past'].map((option) => (
                     <MenuItem key={option} sx={{ height: '35px' }} onClick={() => handleToggle(option)}>
                         <Checkbox checked={checkedItems.indexOf(option) !== -1} />
                         <ListItemText primary={option} primaryTypographyProps={{ fontWeight: 'light', fontSize: '14px' }} />
                     </MenuItem>
                 ))}
 
-
                 <Divider />
 
-                {/* Heading 3 */}
                 <MenuItem sx={{ width: '300px', display: 'flex' }}>
                     <Container disableGutters sx={{ paddingLeft: '12px' }}>
                         <Typography variant="h6" sx={{ fontSize: '16px', fontWeight: 'light' }}>Level</Typography>
                     </Container>
                 </MenuItem>
 
-                {/* Checkboxes under Heading 3 */}
                 {['Easy', 'Medium', 'Hard'].map((option) => (
                     <MenuItem key={option} sx={{ height: '35px' }} onClick={() => handleToggle(option)} >
                         <Checkbox checked={checkedItems.indexOf(option) !== -1} />
@@ -96,33 +110,15 @@ export default function DropdownWithBackdrop() {
             </Menu>
 
             {open && (
-                <>
-                    {/* Backdrop to darken the background */}
-                    <Backdrop
-                        sx={{
-                            zIndex: (theme) => theme.zIndex.modal - 1, // Make sure it stays behind the menu
-                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                        }}
-                        open={true}
-                        onClick={handleClose}
-                    />
-                    {/* Optional: You can use a Box for a similar effect */}
-                    {/* <Box
-            sx={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              zIndex: (theme) => theme.zIndex.modal - 1,
-            }}
-            onClick={handleClose}
-          /> */}
-                </>
+                <Backdrop
+                    sx={{
+                        zIndex: (theme) => theme.zIndex.modal - 1,
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    }}
+                    open={true}
+                    onClick={handleClose}
+                />
             )}
         </>
     );
 }
-
-
