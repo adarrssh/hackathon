ac505ada83fd5bd1f8ede53ec001e99e5276f7af
adarrssh <adarsh00502@gmail.com>
2024-09-05 11:45:35 +0530

    refactor:refactor

--- src/components/Challenges/DatePicker/index.js
+++ src/components/Challenges/DatePicker/index.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TextField } from '@mui/material';
-import './index.css'
+import * as React from "react";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TextField } from "@mui/material";
+import "./index.css";
 
 // Extend dayjs with the customParseFormat plugin
 dayjs.extend(customParseFormat);
@@ -20,19 +20,19 @@ export default function ChallengeDatePicker() {
 
   // Function to format the date
   const formatDate = (date) => {
-    if (!date) return '';
-    return dayjs(date).format('Do MMM\'YY hh:mm A');
+    if (!date) return "";
+    return dayjs(date).format("Do MMM'YY hh:mm A");
   };
 
   return (
-    <LocalizationProvider   dateAdapter={AdapterDayjs}>
-        <DatePicker
-        className='bb'
-          value={value}
-          onChange={handleDateChange}
-
-          renderInput={(params) => <TextField {...params} />}
-        />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        className="bb"
+        value={value}
+        onChange={handleDateChange}
+        sx={{ position: "absolute", bottom: "1px", left: "25px" }}
+        renderInput={(params) => <TextField {...params} />}
+      />
     </LocalizationProvider>
   );
 }
--- src/components/Challenges/index.js
+++ src/components/Challenges/index.js
@@ -2,84 +2,83 @@ import { Button, Container, TextareaAutosize } from "@mui/material";
 
 import React from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import search from '../../assets/image/carbon_search.png'
-import SearchIcon from '@mui/icons-material/Search';
+import search from "../../assets/image/carbon_search.png";
+import SearchIcon from "@mui/icons-material/Search";
 import DropdownWithBackdrop from "./DropDownButton";
 
 const index = ({ setHackathonArr }) => {
-    return (
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#002A3B",
+        paddingTop: "40px",
+        paddingBottom: "40px",
+      }}
+    >
+      <Container
+        sx={{
+          textAlign: "center",
+          color: "white",
+          fontSize: "28px",
+          fontWeight: "bold",
+        }}
+      >
+        Explore Challenges
+      </Container>
+
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+
+          paddingTop: "60px",
+          paddingBottom: "40px",
+        }}
+      >
         <Container
-            maxWidth={false}
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                backgroundColor: "#002A3B",
-                paddingTop: "40px",
-                paddingBottom: "40px",
-            }}
+          sx={{
+            display: "flex",
+            // justifyContent:'center',
+            alignItems: "center",
+            backgroundColor: "#FFFFFF",
+            // backgroundColor: "yellow",
+            width: "85%",
+            borderRadius: "12px",
+            height: "50px",
+          }}
         >
-            <Container
-                sx={{
-                    textAlign: "center",
-                    color: "white",
-                    fontSize: "28px",
-                    fontWeight: "bold",
-                }}
-            >
-                Explore Challenges
-            </Container>
-
-            <Container
-                sx={{
-                    display: "flex",
-                    flexDirection: "row",
-
-                    paddingTop: "60px",
-                    paddingBottom: "40px",
-                }}
-            >
-                <Container
-                    sx={{
-                        display: "flex",
-                        // justifyContent:'center',
-                        alignItems: "center",
-                        backgroundColor: '#FFFFFF',
-                        // backgroundColor: "yellow",
-                        width: "85%",
-                        borderRadius: "12px",
-                        height: "50px",
-                    }}
-                >
-
-                    <img src={search} alt="" />
-                    <textarea
-                        placeholder="search"
-                        style={{
-                            // backgroundColor:"yellow",
-                            overflow:'hidden',
-                            marginLeft:'10px',
-                            color: "#858585",
-                            fontSize: "18px",
-                            fontStyle: "normal",
-                            fontWeight: 300,
-                            height:'20px',
-                            width:'90%',
-                            border: "none",
-                            outline: "none", // This removes the border color on focus
-                            resize: "none", // This disables the resize functionality
-                        }}
-                    />
-                </Container>
-                <Container
-                    sx={{
-                        width: "15%",
-                    }}
-                >
-                    <DropdownWithBackdrop setHackathonArr={setHackathonArr} />
-                </Container>
-            </Container>
+          <img src={search} alt="" />
+          <textarea
+            placeholder="search"
+            style={{
+              // backgroundColor:"yellow",
+              overflow: "hidden",
+              marginLeft: "10px",
+              color: "#858585",
+              fontSize: "18px",
+              fontStyle: "normal",
+              fontWeight: 300,
+              height: "20px",
+              width: "90%",
+              border: "none",
+              outline: "none", // This removes the border color on focus
+              resize: "none", // This disables the resize functionality
+            }}
+          />
         </Container>
-    );
+        <Container
+          sx={{
+            width: "15%",
+          }}
+        >
+          <DropdownWithBackdrop setHackathonArr={setHackathonArr} />
+        </Container>
+      </Container>
+    </Container>
+  );
 };
 
 export default index;
